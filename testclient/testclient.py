14ab3745bb6853b49045de8093d8bc75d1e40ce5
notorious.no@gmail.com <notorious.no@gmail.com>
2016-10-21 09:21:51 -0400

    chicken scratch ideas

--- testclient/testclient.py
+++ testclient/testclient.py
@@ -17,15 +17,38 @@ class TestClient(object):
         fn_name, url_vars = self.klein_map.match(url_endpoint, method=method)
         resource = self.resourceFactory()
         result = self.klein_endpoints[fn_name](self.klein_app._instance, resource, **url_vars)
-        if not isinstance(result, (bytes, str, defer.Deferred)):
-            raise ValueError('@TODO Validate returned')
-        return result, resource
 
+        if isinstance(result, (defer.Deferred)):
+            response = Response(notifyFinish=resource.notifyFinish(), done=False)
+            result.addCallback(self.setContent, response)
+            result.addCallback(lambda x, y: y.callback(None), response.notifyFinish)
+        elif isinstance(result, (str, bytes)):
+            resource.notifyFinish().called = True
+            response = Response(resource.notifyFinish(), content=result, responseCode=resource.responseCode)
+        else:
+            raise ValueError('@TODO Validate returned')
+
+        return response
+
+    def setContent(self, content, response):
+        if not isinstance(content, (str, bytes)):
+            raise ValueError('@TODO')
+
+        response.content = content
+
+
+@attr.s
 class Response(object):
-    pass
+    notifyFinish = attr.ib()
+    content = attr.ib(default=None)
+    responseCode = attr.ib(default=200)
+    done = attr.ib(default=True)
+
 
 if __name__ == '__main__':
     from klein import Klein
+    from twisted.internet import task, reactor
+
     klein_app = Klein()
     @klein_app.route('/')
     def home(request):
@@ -38,8 +61,23 @@ if __name__ == '__main__':
         yield
         return 'inline hello'
 
+    @klein_app.route('/deferred')
+    def testDeferred(request):
+        return task.deferLater(reactor, 3, lambda: 'Hey Earth!')
+
     tester = TestClient(klein_app, 'http://notorious.no')
-    content, resource = tester.request('GET', '/')
-    print(resource.responseCode)
-    content, resource = tester.request('GET', '/inline')
-    print(resource.responseCode)
+    response = tester.request('GET', '/')
+    print(response.responseCode)
+
+    response = tester.request('GET', '/inline')
+    print(response.responseCode)
+
+    def asyncTest():
+        response = tester.request('GET', '/deferred')
+        response.notifyFinish.addCallback(lambda x, y: y.content, response)
+        response.notifyFinish.addCallback(print)
+        return response.notifyFinish
+
+    asyncTest()
+
+    reactor.run()
