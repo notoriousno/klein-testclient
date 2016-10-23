331bfee8e7724672a9291b44857b740a8a1a45c2
notorious.no@gmail.com <notorious.no@gmail.com>
2016-10-23 11:52:50 -0400

    this is pretty much the hard way

--- testclient/testclient.py
+++ testclient/testclient.py
@@ -1,9 +1,11 @@
+from io import BytesIO
+
 import attr
 from twisted.internet import defer
 from twisted.test import proto_helpers
 from twisted.web.http import unquote
 from twisted.web.test.requesthelper import DummyRequest, DummyChannel
-from twisted.web.server import Request
+from twisted.web import server
 
 
 #@attr.s
@@ -16,7 +18,8 @@ class TestClient(object):
 
     def buildRequest(self):
         request = Request(DummyChannel())
-        # request = Request(proto_helpers.StringTransport())
+        # request = server.Request(proto_helpers.StringTransport())
+        request.clientproto = b'HTTP/1.1'
         request.setHeader('Accept', '*/*')
         request.setHeader('User-Agent', 'klien')
         return request
@@ -42,11 +45,13 @@ class TestClient(object):
         url_path = url_endpoint.encode('utf-8')
 
         request = self.buildRequest()
-        request.method = method.encode('utf-8')
+        # request.method = method.encode('utf-8')
         request.prepath = []
         request.postpath = list(map(unquote, url_path[1:].split(b'/')))
         request.path = url_path
         request.setHost(self.hostname, self.port)
+        # request.content = BytesIO()
+        # request.requestReceived(method.encode('utf-8'), url_path, b'HTTP/1.1')
 
         if content:
             self.setContent(request, content)
@@ -57,9 +62,26 @@ class TestClient(object):
         if headers:
             self.setHeaders(request, headers)
 
+        def displayContent(result, req):
+            print('Catching the finishing of a request must be done before kleinapp.render()')
+            print('Result: {0}'.format(req.klein_content))
+        request.notifyFinish().addCallback(displayContent, request)
+
         result = self.klein_app.resource().render(request)
+
         return request
 
+
+# class Request(server.Request):
+class Request(server.Request):
+    def _cleanup(self):
+        self.channel.transport.written.seek(0)
+        self.klein_content = self.channel.transport.written.read()
+        # import pdb;pdb.set_trace()
+        super(Request, self)._cleanup()
+
+    def isSecure(self):
+        return False
 
 class Response(object):
     notifyFinish = attr.ib()
@@ -68,19 +90,26 @@ class Response(object):
     done = attr.ib(default=True)
 
 
-if __name__ == '__main__':
+def main():
     from klein import Klein
     from twisted.internet import task, reactor
 
     app = Klein()
     @app.route('/path/<path:url_path>')
     def urlPath(request, url_path):
+        request.setResponseCode(400)
         return 'hello'
 
     @app.route('/defer')
     def deferred(request):
+        reactor.callLater(5, request.finish)
         return defer.Deferred()
 
     testClient = TestClient(app)
-    print(testClient.fetch('', '/path/abc/123').transport.written.read())
-    print(testClient.fetch('', '/defer'))
+    req = testClient.fetch('GET', '/path/how/to')
+    # req = testClient.fetch('GET', '/defer')
+
+
+from twisted.internet import task, reactor
+main()
+# reactor.run()
