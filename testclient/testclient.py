e77f3d22cd9a9feb1dafb0994fb44157094ac4ea
notorious.no@gmail.com <notorious.no@gmail.com>
2016-10-22 20:29:45 -0400

    more experimenting

--- testclient/testclient.py
+++ testclient/testclient.py
@@ -1,43 +1,66 @@
 import attr
 from twisted.internet import defer
+from twisted.test import proto_helpers
+from twisted.web.http import unquote
+from twisted.web.test.requesthelper import DummyRequest, DummyChannel
+from twisted.web.server import Request
+
 
 #@attr.s
 class TestClient(object):
 
-    def __init__(self, klein_app, server_name=''):
+    def __init__(self, klein_app, hostname='', port=8000):
         self.klein_app = klein_app
-        self.klein_map = klein_app.url_map.bind(server_name)
-        self.klein_endpoints = klein_app.endpoints
+        self.hostname = hostname.encode('utf-8')
+        self.port = port
 
-    def resourceFactory(self):
-        from twisted.web.test.requesthelper import DummyRequest, DummyChannel
-        return DummyRequest(DummyChannel())
+    def buildRequest(self):
+        request = Request(DummyChannel())
+        # request = Request(proto_helpers.StringTransport())
+        request.setHeader('Accept', '*/*')
+        request.setHeader('User-Agent', 'klien')
+        return request
 
-    def request(self, method, url_endpoint, body=None, params=None, headers=None):
-        fn_name, url_vars = self.klein_map.match(url_endpoint, method=method)
-        resource = self.resourceFactory()
-        result = self.klein_endpoints[fn_name](self.klein_app._instance, resource, **url_vars)
+    def setArgs(self, request_args, user_args):
+        for key, value in user_args.items():
+            try:
+                key = key.encode('utf-8')
+            except:
+                pass
 
-        if isinstance(result, (defer.Deferred)):
-            response = Response(notifyFinish=resource.notifyFinish(), done=False)
-            result.addCallback(self.setContent, response)
-            result.addCallback(lambda x, y: y.callback(None), response.notifyFinish)
-        elif isinstance(result, (str, bytes)):
-            resource.notifyFinish().called = True
-            response = Response(resource.notifyFinish(), content=result, responseCode=resource.responseCode)
-        else:
-            raise ValueError('@TODO Validate returned')
+            request_args[key] = value if isinstance(value, list) else [value]
 
-        return response
+    def setContent(self, request, content):
+        request.setHeader('content-length', len(content))
+        request.content = content
 
-    def setContent(self, content, response):
-        if not isinstance(content, (str, bytes)):
-            raise ValueError('@TODO')
+    def setHeaders(self, request, headers):
+        for key, val in headers.items():
+            request.setHeader(key, val)
 
-        response.content = content
+    def fetch(self, method, url_endpoint, content=None, args=None, headers=None):
+        url_path = url_endpoint.encode('utf-8')
+
+        request = self.buildRequest()
+        request.method = method.encode('utf-8')
+        request.prepath = []
+        request.postpath = list(map(unquote, url_path[1:].split(b'/')))
+        request.path = url_path
+        request.setHost(self.hostname, self.port)
+
+        if content:
+            self.setContent(request, content)
+
+        if args:
+            self.setArgs(request.args, args)
+
+        if headers:
+            self.setHeaders(request, headers)
+
+        result = self.klein_app.resource().render(request)
+        return request
 
 
-@attr.s
 class Response(object):
     notifyFinish = attr.ib()
     content = attr.ib(default=None)
@@ -49,35 +72,15 @@ if __name__ == '__main__':
     from klein import Klein
     from twisted.internet import task, reactor
 
-    klein_app = Klein()
-    @klein_app.route('/')
-    def home(request):
-        request.setResponseCode(799)
-        return 'hello world'
+    app = Klein()
+    @app.route('/path/<path:url_path>')
+    def urlPath(request, url_path):
+        return 'hello'
 
-    @klein_app.route('/inline')
-    @defer.inlineCallbacks
-    def inline_cb(request):
-        yield
-        return 'inline hello'
+    @app.route('/defer')
+    def deferred(request):
+        return defer.Deferred()
 
-    @klein_app.route('/deferred')
-    def testDeferred(request):
-        return task.deferLater(reactor, 3, lambda: 'Hey Earth!')
-
-    tester = TestClient(klein_app, 'http://notorious.no')
-    response = tester.request('GET', '/')
-    print(response.responseCode)
-
-    response = tester.request('GET', '/inline')
-    print(response.responseCode)
-
-    def asyncTest():
-        response = tester.request('GET', '/deferred')
-        response.notifyFinish.addCallback(lambda x, y: y.content, response)
-        response.notifyFinish.addCallback(print)
-        return response.notifyFinish
-
-    asyncTest()
-
-    reactor.run()
+    testClient = TestClient(app)
+    print(testClient.fetch('', '/path/abc/123').transport.written.read())
+    print(testClient.fetch('', '/defer'))
